21c7f7efba8f0cdd3fae3a7755dcebd76052dfda
valentinbreiz <valentinbreiz@gmail.com>
2023-06-04 23:30:55 +0200

    Add error handling

--- installer/main.py
+++ installer/main.py
@@ -3,14 +3,17 @@
 # https://github.com/ParthJadhav/Tkinter-Designer
 
 
+import ipaddress
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import messagebox
 from generator import generate
 import json
 import os
 import sys
 import threading
 import webbrowser
+import re
 
 OUTPUT_PATH = Path(__file__).parent
 
@@ -50,6 +53,88 @@ window = Tk()
 
 window.geometry("862x519")
 window.configure(bg = "#003061")
+
+def is_valid_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def is_valid_cidr(ip):
+    try:
+        ipaddress.ip_network(ip, strict=False)
+        return True
+    except ValueError:
+        return False
+
+def is_same_subnet(cidr_ips):
+    try:
+        # Séparation des adresses IP
+        ips = [ip.strip() for ip in cidr_ips.split(",")]
+        
+        # Vérification de chaque adresse IP
+        for ip in ips:
+            if not ipaddress.ip_address(ip):
+                return False
+        
+        # Création du sous-réseau à partir de la première adresse IP
+        network = ipaddress.ip_network(ips[0] + '/24', strict=False)
+        
+        # Vérification de chaque adresse IP dans le sous-réseau
+        for ip in ips[1:]:
+            if ipaddress.ip_address(ip) not in network:
+                return False
+        
+        # Toutes les adresses IP sont valides et dans le même sous-réseau
+        return True
+        
+    except ValueError:
+        # L'adresse IP n'est pas valide
+        return False
+
+def is_valid_ip_list(ip_list):
+    # Si c'est une seule IP
+    if ',' not in ip_list:
+        try:
+            ipaddress.ip_address(ip_list)
+            return True
+        except ValueError:
+            return False
+    # Si c'est une liste d'IPs
+    else:
+        ips = ip_list.split(',')
+        for ip in ips:
+            ip = ip.strip() # Enlever les espaces avant et après
+            try:
+                ipaddress.ip_address(ip)
+            except ValueError:
+                return False
+    return True
+
+def is_valid_port(port):
+    try:
+        port = int(port)  # Convertit la chaîne en nombre entier
+        return 1 <= port <= 65535
+    except ValueError:
+        return False
+
+def is_valid_email(email):
+    # Expression régulière pour vérifier l'adresse e-mail
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return bool(re.match(pattern, email))
+
+def is_dockerfile_path(path):
+    # Vérifie si le chemin existe
+    if not os.path.exists(path):
+        return False
+    
+    # Si le mot 'Dockerfile' n'est pas dans le chemin, ajoute le à la fin
+    if not path.endswith('Dockerfile'):
+        path = os.path.join(path, 'Dockerfile')
+    
+    # Vérifie si le Dockerfile existe dans le chemin
+    return os.path.isfile(path)
 
 def page1():
     canvas = Canvas(
@@ -184,7 +269,7 @@ def page1():
     window.mainloop()
 
 
-def page2():
+def page2(): 
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -403,6 +488,10 @@ def page2():
 
 
 def page3():
+    if not is_valid_cidr(page2_entry_1.get()):
+        messagebox.showerror("Erreur.", "Ceci n'est pas une adresse IP.")
+        page2()
+
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -587,6 +676,14 @@ def page3():
     window.mainloop()
 
 def page4():
+    if not is_valid_ip_list(page3_entry_2.get()):
+        messagebox.showerror("Erreur.", "Le format de la liste est invalide.")
+        page3()
+    
+    if not is_same_subnet(page3_entry_2.get()):
+        messagebox.showerror("Erreur.", "Au moins une IP n'est pas dans le sous réseau: " + page2_entry_1.get())
+        page3()
+
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -762,6 +859,7 @@ def page4():
         fg="#000716",
         highlightthickness=0
     )
+    page4_entry_2.insert('1', '21')
     page4_entry_2.place(
         x=486.0,
         y=241.0,
@@ -771,6 +869,14 @@ def page4():
     window.mainloop()
 
 def page5():
+    if not is_valid_ip_list(page4_entry_1.get()):
+        messagebox.showerror("Erreur.", "Adresse IP invalide.")
+        page4()
+
+    if not is_valid_port(page4_entry_2.get()):
+        messagebox.showerror("Erreur.", "Port invalide.")
+        page4()
+
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -887,6 +993,10 @@ def page5():
     window.mainloop()
 
 def page6():
+    if not is_valid_email(page5_entry_2.get()):
+        messagebox.showerror("Erreur.", "E-mail invalide.")
+        page5()
+
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -989,6 +1099,10 @@ def redirect_output(text_widget):
     sys.stdout = StdoutRedirector(text_widget)
 
 def page7():
+    if not is_dockerfile_path(page6_entry_1.get()):
+        messagebox.showerror("Erreur.", "Chemin du Dockerfile invalide.")
+        page6()
+
     canvas = Canvas(
         window,
         bg = "#003061",
@@ -1173,10 +1287,19 @@ def show_button(widget):
     # This will recover the widget from toplevel
     widget.pack()
 
+def add_dockerfile_to_path(path):
+    # Si le mot 'Dockerfile' n'est pas dans le chemin, l'ajoute à la fin
+    if not path.endswith('Dockerfile'):
+        path = os.path.join(path, 'Dockerfile')
+    
+    return path
+
 def install():
     # Définir les options de configuration
 
     print('Creating config...')
+    
+    dockerfile = add_dockerfile_to_path(page6_entry_1.get())
 
     config = {
         'dummy_pc': {
@@ -1189,7 +1312,7 @@ def install():
         },
         "interface": page2_entry_2.get(),
         "subnet": page2_entry_1.get(),
-        "dockerfile": page6_entry_1.get()
+        "dockerfile": dockerfile
     }
 
     # Créer le dossier 'build' s'il n'existe pas
