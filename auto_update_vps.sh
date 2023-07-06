6098afc3e3bbb4a5f22f47f9a582a00de5bbbf09
AdrienRoco <adrien@rocourt.com>
2023-07-06 18:00:38 +0000

    update script

--- auto_update_vps.sh
+++ auto_update_vps.sh
@@ -5,25 +5,26 @@ docker_file="docker-compose-prod.yml"
 docker_args="-d --build"
 docker_image="honeypot"
 github_repo="https://github.com/Le-Pot-de-Miel/Honeypot.git"
+times=$(date +"%Y-%m-%d %T")
 
-echo "$(date +"%Y-%m-%d %T") Checking for updates on GitHub repository."
+echo "$times: Checking for updates..."
 cd $repo_path
 git fetch origin
 
 if [[ $(git rev-parse HEAD) != $(git rev-parse origin/main) ]]; then
-    echo "$(date +"%Y-%m-%d %T") Update available. Pulling from GitHub repository."
+    echo "$times: Update available..."
     docker-compose -f $repo_path/$docker_file down > /dev/null 2>&1
     git -C "$repo_path" pull origin main
-    docker-compose -f $repo_path/$docker_file up $docker_args > /dev/null 2>&1
-    echo "$(date +"%Y-%m-%d %T") Update complete."
+    echo "$times: Update complete."
 else
-    echo "$(date +"%Y-%m-%d %T") No update available."
+    echo "$times: No update available."
 fi
 
 if [[ $(docker ps -q -f name=$docker_image) == "" ]]; then
-    echo "$(date +"%Y-%m-%d %T") Docker container not running. Starting container."
+    echo "$times: Starting container..."
     docker-compose -f $repo_path/$docker_file up $docker_args > /dev/null 2>&1
-    echo "$(date +"%Y-%m-%d %T") Container started."
+    echo "$times: Container started."
 fi
+echo "$times: Script finished."
 
 exit 0
