7e1db50ce3e838de325acb00a156cd12b0a254f5
AdrienRoco <adrien@rocourt.com>
2023-07-06 18:43:35 +0200

    add timestamp to log

--- auto_update_vps.sh
+++ auto_update_vps.sh
@@ -6,23 +6,23 @@ docker_args="-d --build"
 docker_image="honeypot"
 github_repo="https://github.com/Le-Pot-de-Miel/Honeypot.git"
 
-echo "Checking for updates on GitHub repository."
+echo "$(date +"%Y-%m-%d %T") Checking for updates on GitHub repository."
 git fetch origin
 
 if [[ $(git rev-parse HEAD) != $(git rev-parse origin/main) ]]; then
-    echo "Update available. Pulling from GitHub repository."
+    echo "$(date +"%Y-%m-%d %T") Update available. Pulling from GitHub repository."
     docker-compose -f $repo_path/$docker_file down
     git -C "$repo_path" pull origin main
     docker-compose -f $repo_path/$docker_file up $docker_args
-    echo "Update complete."
+    echo "$(date +"%Y-%m-%d %T") Update complete."
 else
-    echo "No update available."
+    echo "$(date +"%Y-%m-%d %T") No update available."
 fi
 
 if [[ $(docker ps -q -f name=$docker_image) == "" ]]; then
-    echo "Docker container not running. Starting container."
+    echo "$(date +"%Y-%m-%d %T") Docker container not running. Starting container."
     docker-compose -f $repo_path/$docker_file up $docker_args
-    echo "Container started."
+    echo "$(date +"%Y-%m-%d %T") Container started."
 fi
 
 exit 0
