0e7081af75a2c56cbab11bef5a8ce4a4dbd88f9f
AdrienRoco <adrien@rocourt.com>
2023-07-06 19:25:54 +0200

    remove dockercompose output

--- auto_update_vps.sh
+++ auto_update_vps.sh
@@ -12,9 +12,9 @@ git fetch origin
 
 if [[ $(git rev-parse HEAD) != $(git rev-parse origin/main) ]]; then
     echo "$(date +"%Y-%m-%d %T") Update available. Pulling from GitHub repository."
-    docker-compose -f $repo_path/$docker_file down
+    docker-compose -f $repo_path/$docker_file down > /dev/null 2>&1
     git -C "$repo_path" pull origin main
-    docker-compose -f $repo_path/$docker_file up $docker_args
+    docker-compose -f $repo_path/$docker_file up $docker_args > /dev/null 2>&1
     echo "$(date +"%Y-%m-%d %T") Update complete."
 else
     echo "$(date +"%Y-%m-%d %T") No update available."
@@ -22,7 +22,7 @@ fi
 
 if [[ $(docker ps -q -f name=$docker_image) == "" ]]; then
     echo "$(date +"%Y-%m-%d %T") Docker container not running. Starting container."
-    docker-compose -f $repo_path/$docker_file up $docker_args
+    docker-compose -f $repo_path/$docker_file up $docker_args > /dev/null 2>&1
     echo "$(date +"%Y-%m-%d %T") Container started."
 fi
 
