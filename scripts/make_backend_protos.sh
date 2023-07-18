22e0214ad49c96b617460af3d5a3947de3e5386e
Valentin Charbonnier <valentinbreiz@gmail.com>
2023-07-18 17:14:30 +0200

    🐛 Fix Makefile taking only logs proto

--- scripts/make_backend_protos.sh
+++ scripts/make_backend_protos.sh
@@ -9,4 +9,4 @@ cd $BACKEND_DIR
 
 mkdir -p $PROTO_OUTPUT_DIR
 
-yarn grpc_tools_node_protoc --proto_path=$PROTO_DIR --js_out=import_style=commonjs,binary:$PROTO_OUTPUT_DIR --grpc_out=grpc_js:$PROTO_OUTPUT_DIR $PROTO_DIR/logs.proto
+yarn grpc_tools_node_protoc --proto_path=$PROTO_DIR --js_out=import_style=commonjs,binary:$PROTO_OUTPUT_DIR --grpc_out=grpc_js:$PROTO_OUTPUT_DIR $PROTO_DIR/*.proto
