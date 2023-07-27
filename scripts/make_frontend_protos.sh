0a7c599ef342de4019575d35915acb91d23589b4
Valentin Charbonnier <valentinbreiz@gmail.com>
2023-07-27 16:34:24 +0200

    🐛 Fix stubs build

--- scripts/make_frontend_protos.sh
+++ scripts/make_frontend_protos.sh
@@ -2,8 +2,11 @@
 set -e
 
 FRONTEND_DIR=$2
-PROTO_DIR=../Honeypot/$1
+PROTO_DIR=../Honeypot/protos
 PROTO_OUTPUT_DIR=$FRONTEND_DIR/src/protos
+
+PROTOC_GEN_TS_PATH=node_modules/.bin/protoc-gen-ts
+PROTOC_GEN_GRPC_PATH=node_modules/.bin/grpc_tools_node_protoc_plugin
 
 cd $FRONTEND_DIR
 
@@ -11,5 +14,7 @@ mkdir -p $PROTO_OUTPUT_DIR
 
 yarn grpc_tools_node_protoc \
     -I=$PROTO_DIR \
-    --ts_out=service=grpc-node,mode=grpc-js:$PROTO_OUTPUT_DIR \
-    $PROTO_DIR/*.proto
+    --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
+    --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+    --ts_out=grpc_js:$PROTO_OUTPUT_DIR \
+    "$PROTO_DIR"/*.proto
