fd8c9c61da609f262710fc7226134934e19819c7
valentinbreiz <valentinbreiz@gmail.com>
2023-09-16 19:50:07 +0200

    🐛 Block all protocols + use default interface

--- scripts/start_honeybrain.sh
+++ scripts/start_honeybrain.sh
@@ -11,8 +11,11 @@ docker compose -p honeybrain -f $DOCKER_COMPOSE_FILE up -d
 
 echo "⌛ Getting network interface..."
 
-NETWORK_ID=$(docker network ls --filter name=honeypot_network --format '{{.ID}}')
-export NETWORK_INTERFACE=br-${NETWORK_ID}
+#NETWORK_ID=$(docker network ls --filter name=honeypot_network --format '{{.ID}}')
+#export NETWORK_INTERFACE=br-${NETWORK_ID}
+
+NETWORK_ID=$(ip route | grep default | awk '{print $5}')
+export NETWORK_INTERFACE=${NETWORK_ID}
 
 echo "✅ Now spying network honeypot_network ($NETWORK_INTERFACE)"
 
